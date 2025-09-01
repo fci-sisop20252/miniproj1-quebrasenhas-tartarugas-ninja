d91d9bb1e596576a5fd08bdb9f90278fbae08860
Marcus Baena <marcusbaena@gmail.com>
2025-09-01 22:32:04 +0000

    todo 1

--- src/worker.c
+++ src/worker.c
@@ -32,7 +32,23 @@
  * @return 1 se incrementou com sucesso, 0 se chegou ao limite (overflow)
  */
 int increment_password(char *password, const char *charset, int charset_len, int password_len) {
+    for (int i = password_len - 1; i >= 0; i--) {
+        int cont = 0;
+        while (cont < charset_len && charset[cont] != password[i])
+            cont++;
+        
+        cont++; // incrementa
+        
+        if (cont < charset_len) {
+            password[i] = charset[cont]; // atualiza e retorna
+            return 1;
+        } else {
+            password[i] = charset[0]; // estouro, continua
+        }
+    }
     
+    return 0;
+ } // todos estouraram
     // TODO 1: Implementar o algoritmo de incremento de senha
     // OBJETIVO: Incrementar senha como um contador (ex: aaa -> aab -> aac -> aad...)
     // DICA: Começar do último caractere, como somar 1 em um número
@@ -45,9 +61,8 @@ int increment_password(char *password, const char *charset, int charset_len, int
     // - Se não estourou: atualizar caractere e retornar 1
     // - Se estourou: definir como primeiro caractere e continuar loop
     // - Se todos estouraram: retornar 0 (fim do espaço)
-    
-    return 0;  // SUBSTITUA por sua implementação
-}
+     // SUBSTITUA por sua implementação
+
 
 /**
  * Compara duas senhas lexicograficamente
